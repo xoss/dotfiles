838cf69130141da9eebc1016380bd2f4b426d8bf
Martin Kühn <martin.kuehn@amadeus.com>
2019-01-16 16:14:30 +0100

    Update install.sh
    KeyBoard Repeat speed set to decent level

--- install.sh
+++ install.sh
@@ -607,8 +607,8 @@ running "Disable press-and-hold for keys in favor of key repeat"
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false;ok
 
 running "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 2
-defaults write NSGlobalDomain InitialKeyRepeat -int 10;ok
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 5;ok
 
 running "Set language and text formats (english/US)"
 defaults write NSGlobalDomain AppleLanguages -array "en"
